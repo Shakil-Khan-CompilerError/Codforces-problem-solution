21e4c74ef104f9d8265f66f3a20c1b234a60af47
Shakil Khan <shakilkhan116797@gmail.com>
2023-05-21 21:51:20 +0600

    Add files via upload

---
+++ Bit++.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, X=0;
+    string s;
+    cin>>n;
+    for(int i = 0; i < n; i++)
+    {
+        cin>>s;
+        if(s[1]=='+')
+        {
+            X++;
+        }
+        else
+        {
+            X--;
+        }
+    }
+    cout<<X<<endl;
+}
+
