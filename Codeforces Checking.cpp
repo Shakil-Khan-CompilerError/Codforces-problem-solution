06ed88a9db807f6cca80ef17bc127a42d72dddae
Shakil Khan <shakilkhan116797@gmail.com>
2023-05-21 22:00:17 +0600

    Add files via upload

---
+++ Codeforces Checking.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        char c;
+
+        cin >> c;
+        if (c == 'c' || c == 'o' || c == 'd' || c == 'e' || c == 'f' || c == 'r' || c == 's')
+        {
+            cout << "YES" << endl;
+        }
+        else
+            cout << "NO" << endl;
+    }
+    return 0;
+}
