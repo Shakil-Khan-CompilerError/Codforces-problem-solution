2cd4a3ce9eb1148678b6367bd072f9faf5233ccc
Shakil Khan <shakilkhan116797@gmail.com>
2023-08-20 22:01:43 +0600

    Add files via upload

---
+++ Anton and Polyhedrons.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    int n,sum=0;
+    cin>>n;
+    while(n--)
+    {
+        cin>>s;
+        if(s[0]=='T')sum+=4;
+        if(s[0]=='C')sum+=6;
+        if(s[0]=='O')sum+=8;
+        if(s[0]=='D')sum+=12;
+        if(s[0]=='I')sum+=20;
+    }
+    cout<<sum;
+    return 0;
+}
+
